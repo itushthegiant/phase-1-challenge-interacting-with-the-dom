58017e0d520c0300d7b85261ced959a456fd3c65
etamar <etamar.levy@gmail.com>
2021-04-08 20:47:07 -0400

    comment

--- js/challenge.js
+++ js/challenge.js
@@ -20,22 +20,55 @@ document.addEventListener('DOMContentLoaded', () => {
 ////// "like" button //////
     const like = document.getElementById('heart');
     const ul = document.querySelector(".likes");
-    let numOfLikes = 1
     like.addEventListener('click', () => {
         let li;
         if (document.querySelector(`[data-num='${counter.innerText}']`)) {
             li = document.querySelector(`[data-num='${counter.innerText}']`)
-            li.innerText = `${counter.innerText} have ${numOfLikes++} likes!`
+            li.innerText = `${counter.innerText} have ${li.dataset.count++} likes!`
         } else {
             li = document.createElement('li');
             ul.appendChild(li);
             li.setAttribute("data-num", counter.innerText);
+            li.setAttribute("data-count", 1)
             li.innerText = `${counter.innerText} have 1 likes!`;
         }
     })
 
 
+////// "pause" button //////    
+    const pause = document.getElementById('pause');
+    pause.addEventListener('click', () => {
+        if (pause.innerText === "pause") {
+            like.disabled = true;
+            minus.disabled = true;
+            plus.disabled = true;
+            clearInterval(startTime);
+            pause.innerText = "resume";
+        } else {
+            like.disabled = false;
+            minus.disabled = false;
+            plus.disabled = false;
+            pause.innerText = "pause";
+            setInterval(function() {
+               counter.innerText++
+            }, 1000) 
+        }
+    })
 
+
+////// "comments" //////
+    const list = document.getElementById('list');
+    const form = document.getElementById("comment-form");
+    const input = document.getElementById("comment-input");
+    const comment = input.value;
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const uList = document.createElement('ul');
+        const li = document.createElement('li');
+        list.appendChild(uList);
+        li.appendChild(document.createTextNode(comment));
+        list.appendChild(li);
+    })
 
 
 
